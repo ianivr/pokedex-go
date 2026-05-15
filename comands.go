da826d834b47659f1f6370181326ea3914272a67
ianivr <ianivrubinstein@gmail.com>
2026-05-15 04:17:49 +0000

    Implement command handling for help and exit in the Pokedex

---
+++ comands.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    help,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    exit,
+		},
+	}
+}
+
+func exit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func help() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -16,8 +16,16 @@ func main() {
 		input := scanner.Text()
 
 		inputCleaned := cleanInput(input)
-		firstWord := inputCleaned[0]
 
-		fmt.Println("Your command was:", firstWord)
+		cmd, exists := commands[inputCleaned[0]]
+		if exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Printf("Error executing command: %v\n", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
+
 }
