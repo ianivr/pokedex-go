40f21cf2702de8a5e7ecccf9625cc74bb1b5ace2
ianivr <ianivrubinstein@gmail.com>
2026-05-26 20:30:12 +0000

    Refactor Client initialization: add cache support with configurable cache interval

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -2,14 +2,19 @@ package pokeapi
 
 import (
 	"net/http"
+	"time"
+
+	"github.com/ianivr/pokedex-go/internal/pokecache"
 )
 
 type Client struct {
 	httpClient http.Client
+	cache      pokecache.Cache
 }
 
-func NewClient() Client {
+func NewClient(cacheInterval time.Duration) Client {
 	return Client{
 		httpClient: http.Client{},
+		cache:      pokecache.NewCache(cacheInterval),
 	}
 }
