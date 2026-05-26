76ce0c3f3054fcdcf6e8ae660ee5f709d3f21653
ianivr <ianivrubinstein@gmail.com>
2026-05-26 20:29:22 +0000

    Consolidate location-related types into a single file and remove deprecated types_locations.go

---
+++ internal/pokeapi/types.go
@@ -0,0 +1,45 @@
+package pokeapi
+
+type LocationListResponse struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+type LocationPokemonResponse struct {
+	ID       int `json:"id"`
+	Location struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Name              string `json:"name"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
+}
+
+type Pokemon struct {
+	BaseExperience int    `json:"base_experience"`
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	Height         int    `json:"height"`
+	Weight         int    `json:"weight"`
+	Stats          []struct {
+		BaseStat int `json:"base_stat"`
+		Stat     struct {
+			Name string `json:"name"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+		} `json:"type"`
+	} `json:"types"`
+}
